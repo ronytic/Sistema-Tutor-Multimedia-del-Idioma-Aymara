e3ddec7728a632e674d0c4f7491b1ef8ed519446
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-06-25 12:00:25 -0400

    Corregido Login

--- login/check.php
+++ login/check.php
@@ -6,6 +6,8 @@ $subfolder=$folderFile[1];
 
 include_once($_SERVER['DOCUMENT_ROOT']."/".$subfolder."/config.php");
 if(!(isset($_SESSION["login"]) && $_SESSION['login']==1)){
-	header("Location:$url$directory/login/?u=".$_SERVER['PHP_SELF']);
+    if(!isset($revisar)){
+	    header("Location:$url$directory/login/?u=".$_SERVER['PHP_SELF']);
+    }
 }
 ?>
