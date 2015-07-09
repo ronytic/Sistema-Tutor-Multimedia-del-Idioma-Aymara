f277e9e6ff21ddb535f736d224ec0d91d179e0b3
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-07-09 18:23:17 -0400

    Aumentado Respuetas

--- class/respuestas.php
+++ class/respuestas.php
@@ -2,5 +2,10 @@
 include_once("bd.php");
 class respuestas extends bd{
 	var $tabla="respuestas";
+    
+    function nota($codestudiante,$bimestre){
+        $this->campos=array("*,avg(correcta) as nota ");
+        return $this->getRecords("activo=1 and codestudiante=$codestudiante and bimestre=$bimestre","","",0,0,0);
+    }
 }
 ?>
