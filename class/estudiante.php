3792021c6e1ed8e60265121c7ca655071cbce18a
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-07-09 16:39:49 -0400

    Login Estudiante

--- class/estudiante.php
+++ class/estudiante.php
@@ -2,5 +2,9 @@
 include_once("bd.php");
 class estudiante extends bd{
 	var $tabla="estudiante";
+    function loginUsuarios($Usuario,$Password){
+		$this->campos=array("count(*) as Can,codestudiante as codusuarios");	
+		return $this->getRecords("carnet='$Usuario' and paterno='$Password' and activo=1");
+	}
 }
 ?>
