45bfef49fd9f2484e23c44e8a54920fe9f7b2b76
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-07-10 09:47:25 -0400

    Pie de Pagina

--- piepagina.php
+++ piepagina.php
@@ -1,5 +1,25 @@
 	<?php include_once("config.php");?>
     <div class="clear"></div>
+    <div class="grid_12">
+    
+	<div class="usuariocuerpo">
+         <?php if($nivel){?>
+		<?php if($nivel!=4){?>
+        <span class="pequenol">Nombre:</span> <?php echo $us['nombre'];?> | 
+		<span class="pequenol">Usuario:</span> <?php echo $us['usuario'];?> |
+		<span class="pequenol">Hora Acceso:</span> <?php echo $_SESSION['horasesion'];?> |
+		<a href="<?php echo $folder?>usuarios/cambiarp.php?id=<?php echo $_SESSION['idusuario']?>" class="enlaceusuario">Cambiar Contraseña</a>
+        <?php }?>
+		<a href="<?php echo $folder ?>login/logout.php" class="botonplomo">Salir de Administración</a>
+        <?php }else{
+           ?>
+           <marquee>UNIDAD EDUCATIVA DON BOSCO-EL PRADO</marquee>
+           <?php 
+        }?>
+	</div>
+    
+</div>
+<div class="clear"></div>
 	<div class="grid_12"> 
 		<div id="piepag">
             <div class="mensaje">
