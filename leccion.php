164b2b8f186005eb634a71e7e29ca5be9e8efdc4
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2016-07-27 15:38:39 -0400

    leccion

--- leccion.php
+++ leccion.php
@@ -11,44 +11,43 @@ $temas=new temas;
 $tem=$temas->mostrarTodo("codtemas=".$d);
 $tem=array_shift($tem);;
 ?>
-<div class="titulo"><?php echo $tem['nombre']?></div>
+<script>
+    <?php if($tem['descripcion']!=""){?>
+        $(".ninodefecto").slideUp("slow");
+        $(".ninotexto").html("<?php echo $tem['descripcion']?>");
+        $(".nino").slideDown("slow")
+        
+    <?php }else{?>
+        $(".nino").slideUp("slow");
+        $(".ninotexto").html("");
+        $(".ninodefecto").slideDown("slow")
+    <?php }?>
+    <?php if($tem['video']!=""){?>
+        $(".videoenlace").attr("rel","<?php echo $tem['video']?>");
+        $(".video").slideDown("slow")
+        
+    <?php }else{?>
+        $(".video").slideUp("slow");
+        $(".videoenlace").attr("rel","");
+
+    <?php }?>
+</script>
+<div class="titulo3"><?php echo $tem['nombre']?></div>
 <?php if($tem['descripcion']!=""){?>
-<fieldset>
+<?php /*?><fieldset>
 <p><?php echo $tem['descripcion']?></p>
 </fieldset>
+<?php */?>
 <?php
 }
 foreach($cont as $t){
     ?>
-    <div style="width:335px;  display: inline;
-  float: left;">
-        <div class="contenidot">
-            <div class="titulo tituloc" rel="<?php echo $t['codcontenido']?>"><?php    echo $t['nombre'];?></div>
-            <br>
-            <div class="contenidoc" rel="<?php echo $t['codcontenido']?>" style="display:none">
-            <?php
-                if($t['imagen']!=""){
-                ?><img src="contenido/imagen/<?php echo $t['imagen']?>" width="300"><?php	
-                }
-            ?>
-            <br>
-            <?php
-                if($t['audio']!=""){
-                    
-                ?><audio src="contenido/audio/<?php echo $t['audio']?>" controls width="300"></audio><?php
-                }
-			?>
-            <br>
-            <?php
-                if($t['video']!=""){
-                    
-                ?><video src="contenido/video/<?php echo $t['video']?>" controls width="300" preload="auto"></video><?php
-                }
-            ?>
-            </div>
-        </div>
+    <div style="display: inline; float: left;" class="cuadritos ">
+        <a rel="<?php echo $t['codcontenido']?>" class="boton2" href="#" title="<?php echo $t['descripcion'];?>"><?php echo $t['nombre'];?></a>
     </div>
     <?php
 }
 ?>
 <div class="clear"></div>
+<hr>
+<div class="contenidounico"></div>
